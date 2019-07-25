0e489070cfd38b9d8c8d68aa5a1b46bd6b4f7c53
XasikSharp <53301451+XasikSharp@users.noreply.github.com>
2019-07-25 15:10:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack_Game_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Добрый день, {name}!");
+            Console.WriteLine("Сегодня");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Хотите сыграть в BlackJack?");
+            Console.WriteLine();
+
+            Console.WriteLine("Для начала игры нажмите 'Y' / Для выхода нажмите 'n'");
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.WriteLine( );
+                Console.WriteLine("Правила игры в BlackJack следующие:");
+                Console.WriteLine("1.");
+            }
+            else if (Console.ReadKey().Key == ConsoleKey.N)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Досведос! Приходите в следующий раз!");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Твой выбор не ясен");
+            }
+
+
+
+            Console.WriteLine();
+
+
+            
+
+
+
+            Console.WriteLine("Для выхода нажмите ENTER два раза");
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
